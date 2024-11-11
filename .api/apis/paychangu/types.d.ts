d00f568c8dad64b322435b7b2097ba6cd1720361
Jones Thukuta <141161604+thukutaJones@users.noreply.github.com>
2024-11-11 07:45:44 +0100

    Delete .api directory

--- .api/apis/paychangu/types.d.ts
+++
@@ -1,5 +0,0 @@
-import type { FromSchema } from 'json-schema-to-ts';
-import * as schemas from './schemas';
-export type LevelReferenceBodyParam = FromSchema<typeof schemas.LevelReference.body>;
-export type LevelReferenceResponse200 = FromSchema<typeof schemas.LevelReference.response['200']>;
-export type LevelReferenceResponse400 = FromSchema<typeof schemas.LevelReference.response['400']>;
